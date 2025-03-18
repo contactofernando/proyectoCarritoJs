53bc61e03b18e26886bebe2a55bdabd7f515e278
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-03-18 13:24:24 -0300

    Leer los datos del curso seleccionado y crear un objeto dinamico

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Variables ...
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    // Cuando agregas un curso presionando "Agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso)
+}
+// Funciones ...
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement; 
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+// Leer el contenido HTML al que le dimos click y extraer la informacion del curso.
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    // Crear un objeto con el contenidodel curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(infoCurso)
+}
